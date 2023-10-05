e9f58e655f53b6179cd967c76122dec31d4e9279
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-10-05 17:56:57 +0900

    aws cicd test

--- stepfunctions_deploy.sh
+++ stepfunctions_deploy.sh
@@ -6,6 +6,7 @@ cat ${SM_LIST} | while read line
 do
   SM_NAME=`echo "${line}" | awk -F',' {'print $1'}`
   SM_FILE=`echo "${line}" | awk -F',' {'print $2'}`
+  echo "${SM_NAME} , ${SM_FILE}"
   "aws stepfunctions update-state-machine --state-machine-arn ${SM_NAME} --definition file://stepfunctions/${SM_FILE}"
   if [ $? -ne 0 ]; then
     echo "StepFunctions Deploy Error. StateMachineName=${SM_NAME}."
