e604f9dafc58c28cbad28d56939c3baaa142a424
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-10-05 15:31:11 +0900

    aws cicd test

---
+++ lambda_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#
+FUNC_LIST='func.lst'
+cat ${FUNC_LIST} | while read line
+do
+  FUNC_NAME=`echo "${line}" | awk -F',' {'print $1'}`
+  FUNC_FILE=`echo "${line}" | awk -F',' {'print $2'}`
+  cd lambda/${FUNC_FILE}
+  zip -r lambda.zip ./*
+  aws lambda update-function-code --function-name ${FUNC_NAME} --zip-file fileb://lambda.zip
+  if [$? ne 0]; then
+    echo "Lambda Deploy Error. FunctionName=${FUNC_NAME}."
+    exit 255
+  fi
+  cd ../../
+done
---
+++ stepfunctions_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#
+SM_LIST='statemachine.lst'
+cat ${SM_LIST} | while read line
+do
+  SM_NAME=`echo "${line}" | awk -F',' {'print $1'}`
+  SM_FILE=`echo "${line}" | awk -F',' {'print $2'}`
+  "aws stepfunctions update-state-machine --state-machine-arn ${SM_NAME} --definition file://stepfunctions/${SM_FILE}"
+  if [$? ne 0]; then
+    echo "StepFunctions Deploy Error. StateMachineName=${SM_NAME}."
+    exit 255
+  fi
+done
