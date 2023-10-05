cb81b569ddd3e5748b52059b442cc6ac139e72cb
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-10-05 16:02:48 +0900

    aws cicd test

--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -6,6 +6,7 @@ cat ${FUNC_LIST} | while read line
 do
   FUNC_NAME=`echo "${line}" | awk -F',' {'print $1'}`
   FUNC_FILE=`echo "${line}" | awk -F',' {'print $2'}`
+  echo "${FUNC_NAME} , ${FUNC_FILE}"
   cd lambda/${FUNC_FILE}
   zip -r lambda.zip ./*
   aws lambda update-function-code --function-name ${FUNC_NAME} --zip-file fileb://lambda.zip
