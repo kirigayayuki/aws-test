8b6949f1cf9312c4d039fe8dbeb320b36a69fdbf
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-10-05 16:56:12 +0900

    aws cicd test

--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -10,14 +10,14 @@ do
   cd lambda/${FUNC_FILE}
   if [ $? -ne 0 ]; then
     echo "Lambda No such file or directory. FunctionName=${FUNC_NAME}, DirectoryName=${FUNC_FILE}."
-    break
+    break 255
   fi
   zip -r lambda.zip ./*
   aws lambda update-function-code --function-name ${FUNC_NAME} --zip-file fileb://lambda.zip
   echo "$?"
   if [ $? -ne 0 ]; then
     echo "Lambda Deploy Error. FunctionName=${FUNC_NAME}."
-    break
+    break 255
   fi
   cd ../../
 done
