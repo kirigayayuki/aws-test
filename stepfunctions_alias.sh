d335728d94626504010f5a586ea22a414a9dbfa8
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-10-06 18:11:54 +0900

    aws cicd test

--- stepfunctions_alias.sh
+++ stepfunctions_alias.sh
@@ -11,7 +11,7 @@ do
     echo "StepFunctions Publish Version Error. StateMachineName=${SM_NAME}."
     exit 2
   fi
-  SM_TARGETVER1=$(echo $(aws stepfunctions list-state-machine-versions --state-machine-arn ${SM_NAME} | grep stateMachineVersionArn | head -1 | cut -d : -f 9 | tr -cd 0-9))
+  SM_TARGETVER=$(echo $(aws stepfunctions list-state-machine-versions --state-machine-arn ${SM_NAME} | grep stateMachineVersionArn | head -1 | cut -d : -f 9 | tr -cd 0-9))
   echo "current ver.${SM_CURRENTVER} >> target ver.${SM_TARGETVER}"
   aws stepfunctions update-state-machine-alias --state-machine-alias-arn ${SM_NAME}:prod --routing-configuration stateMachineVersionArn=${SM_NAME}:${SM_TARGETVER},weight=100
   if [ $? -ne 0 ]; then
