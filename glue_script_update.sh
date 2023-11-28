989a83c0e02cd4017f89f6fb38efa36415e40459
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-11-28 10:00:50 +0900

    template fixed.

--- glue_script_update.sh
+++ glue_script_update.sh
@@ -8,10 +8,10 @@ do
   JOB_NAME=`echo "${line}" | awk -F',' {'print $1'}`
   JOB_FILE=`echo "${line}" | awk -F',' {'print $2'}`
   ### file check ###
-  echo "Start checking GlueJobName=${JOB_NAME}, SourceFilePass=${JOB_NAME}/script/${JOB_FILE}."
-  cd ${JOB_NAME}/script
+  echo "Start checking GlueJobName=${JOB_NAME}, SourceFilePass=${JOB_NAME}/${JOB_FILE}."
+  cd ${JOB_NAME}
   if [ $? -ne 0 ]; then
-    echo "Glue Job No such file or directory. JobName(Directory)=${JOB_NAME}, SourceFilePass=${JOB_NAME}/script/${JOB_FILE}."
+    echo "Glue Job No such file or directory. JobName(Directory)=${JOB_NAME}, SourceFilePass=${JOB_NAME}/${JOB_FILE}."
     exit 2
   fi
   ### deploy ###
@@ -21,5 +21,5 @@ do
     echo "Glue Job s3 Upload Error. JobName(Directory)=${JOB_NAME}, Job FileName=${JOB_FILE}."
     exit 2
   fi
-  cd ../../
+  cd ../
 done
