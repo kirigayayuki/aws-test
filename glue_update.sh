b852d925771266e8e6743225485732014385e00e
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2023-12-26 13:10:30 +0900

    script fixed.

--- glue_update.sh
+++ glue_update.sh
@@ -2,34 +2,52 @@
 #
 #
 BUCKET='2175051-sf-cicd-test-bucket/glue/'
-JOB_LIST='glue.lst'
-LIB_FILE='lib/glue_script.py'
+JOB_PREFIX='source/'
+LIB_PREFIX='lib/'
+JOB_LIST='glue_job.lst'
+LIB_LIST='glue_lib.lst'
 
-  ### job library update & deploy ###
-aws s3 cp ${LIB_FILE} s3://${BUCKET}lib/
-
-  ### job list load ###
+#################################################
+###### job library list load & file update ######
+#################################################
+cat ${LIB_LIST} | while read line
+do
+  LIB_FILE=`echo "${line}" | awk -F',' {'print $1'}`
+  echo "Start checking  SourceFilePass=${LIB_FILE}."
+  ls ${LIB_FILE}
+  if [ $? -ne 0 ]; then
+    echo "Glue Job Library File No such file or directory. SourceFilePass=${LIB_FILE}."
+    exit 2
+  fi
+  aws s3 cp ${LIB_FILE} s3://${BUCKET}${LIB_PREFIX}
+  if [ $? -ne 0 ]; then
+    echo "Glue Job Library File s3 Upload Error. SourceFilePass=${LIB_FILE}."
+    exit 2
+  fi
+done
+#################################################
+###### job list load & file update & deploy #####
+#################################################
 cat ${JOB_LIST} | while read line
 do
   JOB_NAME=`echo "${line}" | awk -F',' {'print $1'}`
-  JOB_FILE=`echo "${line}" | awk -F',' {'print $2'}`
-  ### file check ###
-  echo "Start checking GlueJobName=${JOB_NAME}, SourceFilePass=${JOB_NAME}/${JOB_FILE}."
-  cd ${JOB_NAME}
+  JOB_DIR=`echo "${line}" | awk -F',' {'print $2'}`
+  JOB_FILE=`echo "${line}" | awk -F',' {'print $3'}`
+  echo "Start checking GlueJobName=${JOB_NAME}, SourceFilePass=${JOB_DIR}${JOB_FILE}."
+  ls ${JOB_DIR}${JOB_FILE}
   if [ $? -ne 0 ]; then
-    echo "Glue Job No such file or directory. JobName(Directory)=${JOB_NAME}, SourceFilePass=${JOB_NAME}/${JOB_FILE}."
+    echo "Glue Job Script File No such file or directory. SourceFilePass=${JOB_DIR}${JOB_FILE}."
     exit 2
   fi
-  ### job script update & deploy ###
   echo "Start deploying ${JOB_NAME}."  
-  aws s3 cp ${JOB_FILE} s3://${BUCKET}${JOB_NAME}/
+  aws s3 cp ${JOB_DIR}${JOB_FILE} s3://${BUCKET}${JOB_PREFIX}
   if [ $? -ne 0 ]; then
-    echo "Glue Job s3 Upload Error. JobName(Directory)=${JOB_NAME}, Job FileName=${JOB_FILE}."
+    echo "Glue Job Script File s3 Upload Error. SourceFilePass=${JOB_DIR}${JOB_FILE}."
     exit 2
   fi
-  aws glue update-job --cli-input-json file://definition.json
+  aws glue update-job --cli-input-json file://${JOB_DIR}definition.json
   if [ $? -ne 0 ]; then
-    echo "Glue Job Deploy Error. JobName(Directory)=${JOB_NAME}, Job FileName=${JOB_FILE}."
+    echo "Glue Job Deploy Error. GlueJobName=${JOB_NAME}, SourceFilePass=${JOB_DIR}${JOB_FILE}."
     exit 2
   fi
   cd ../
