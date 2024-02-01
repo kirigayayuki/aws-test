66c60ff4da1bec2850ea27eed6bb007f1af9296a
kirigaya <yuki.kirigaya@ctc-g.co.jp>
2024-02-01 14:28:01 +0900

    initial commit.

--- git_connect.sh
+++ git_connect.sh
@@ -22,19 +22,3 @@ interact
 "
 pwd
 ls -l
-cd ./${DST_REPO}
-git remote add torikomi_repo https://git-codecommit.${SRC_REGION}.amazonaws.com/v1/repos/${SRC_REPO}
-expect -c "
-set timeout 10
-spawn git pull torikomi_repo main
-expect \"Username\"
-send \"2175051_appOnly-at-477460359712\n\"
-expect \"Password\"
-send \"c3BgNSqICOdcbP4oxO5p0EDhqTURSm8+w+4+CVFauyQ=\n\"
-interact
-"
-git add .
-git commit -m 'get source repo'
-git remote rm torikomi_repo
-git push origin main
-
